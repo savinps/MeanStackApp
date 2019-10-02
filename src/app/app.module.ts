8b9e375197b4ad21a354296a22b8b6ce85c12535
savinps <savinps@gmail.com>
2019-10-02 15:37:51 -0400

    pushing code

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from "./header/header.component";
+import { AuthInterceptor } from './auth/auth-interceptor';
+import { ErrorInterceptor } from './error-inceptor';
+import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
+import { PostsModule } from './posts/posts.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    PostsModule
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
+  bootstrap: [AppComponent],
+  entryComponents:  [ErrorComponent]
+})
+export class AppModule {}
